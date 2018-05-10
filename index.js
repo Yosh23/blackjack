7b67266354093d35987def0c928ae65ce2935f33
Josh Riches <joshriches@gmail.com>
2018-05-10 09:06:45 -0600

    Shuffling Deck & organized code

--- index.js
+++ index.js
@@ -1,22 +1,43 @@
-
+//card variables
 let cardValue = ['Ace', 'King', 'Queen', 'Jack',
- 'Ten', 'Nine', 'Eight', 'Seven', 'Six', 'Five', 'Four', 'Three', 'Two'];
-
+                 'Ten', 'Nine', 'Eight', 'Seven',
+                 'Six', 'Five', 'Four', 'Three', 'Two'];
 let suits = ['Hearts', 'Diamonds', 'Spades', 'Clubs'];
 
+// DOM variables
 let textArea = document.getElementById('text-area');
 let newGameButton = document.getElementById('new-game-button');
 let hitButton = document.getElementById('hit-button');
 let stayButton = document.getElementById('stay-button');
 
+// Game variables
+let gameStarted = false;
+let gameOver = false;
+let playerWon = false;
+let dealerCards = [];
+let playerCards = [];
+let dealerScore = 0;
+let playerScore = 0;
+let deck = [];
+
 hitButton.style.display = 'none';
 stayButton.style.display = 'none';
+showStatus();
 
 newGameButton.addEventListener('click', function() {
-    textArea.innerText = 'Started...';
+    gameStarted = true;
+    gameOver = false;
+    playerWon = false;
+
+    deck = createDeck();
+    shuffleDeck(deck);
+    dealerCards = [getNextCard(), getNextCard()];
+    playerCards = [getNextCard(), getNextCard()];
+
     newGameButton.style.display = 'none';
     hitButton.style.display = 'inline';
     stayButton.style.display = 'inline';
+    showStatus();
 });
 
 function createDeck() {
@@ -34,17 +55,28 @@ function createDeck() {
     return deck;
 }
 
+function shuffleDeck(deck) {
+    for (let i = 0; i < deck.length; i++) {
+        let swapIdx = Math.trunc(Math.random() * deck.length);
+        let tmp = deck[swapIdx];
+        deck[swapIdx] = deck[i];
+        deck[i] = tmp;
+    }
+}
+
 function getCardString(card) {
     return card.value + " of " + card.suit;
+}
+
+function showStatus() {
+    if (!gameStarted) {
+        textArea.innerText = 'Welcome to BlackJack!'
+        return;
+    }
+
 }
 
 function getNextCard () {
     return deck.shift();
 }
-let deck = createDeck();
-
-let playerCards = [getNextCard(), getNextCard()];
-
-console.log(getCardString(playerCards[0]));
-console.log(getCardString(playerCards[1]));
 // ========================================
